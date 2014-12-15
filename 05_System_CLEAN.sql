d0e8abbb9cbbdad54d34e2ffbf9d1efd5057c62b
Paco Saucedo <fcosaucedo@gmail.com>
2014-12-15 22:15:13 +0100

    Create 05_System_CLEAN.sql

---
+++ 05_System_CLEAN.sql
@@ -0,0 +1,13 @@
+/* ---------------------------------------------------------------
+ * 05_System_CLEAN.sql : clean the resources by dropping the user.
+ *
+ * Author: Francisco Saucedo (http://fcosfc.wordpress.com)
+ *
+ * Versioning:
+ *
+ *    v1.0, 14-12-2014: Initial version.
+ *
+ * License: GNU GPL (http://www.gnu.org/licenses/gpl-3.0.html)
+ * --------------------------------------------------------------- */
+
+drop user testaq cascade;
